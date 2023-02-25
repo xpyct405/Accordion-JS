dce28bd60a91c3ff919be816995ee83e481755bb
Rustam Kuzyaev <xpyct405@yandex.ru>
2023-02-25 10:36:52 +0300

    feat: add +/- signs, add animation, add verification of contains classes

--- app.js
+++ app.js
@@ -4,38 +4,26 @@ let content = document.querySelectorAll('.content');
 for (let i =0; i < accordion.length; i++){
     accordion[i].addEventListener('click', function(){
 
-        //  Вариант 1
-        // accordion[i].classList.toggle('active');
-
-        //  Вариант 2
-        // Remove .active class from all acordion items
-        for (let i = 0; i < accordion.length; i++){
+        // Проверка наличие класса .active у accordion[i], если есть то удалить .active у accordion[i] и content[i], чтоб по двойному клику вкладка открывалась и закрывалась
+        if (accordion[i].classList.contains('active')){
             accordion[i].classList.remove('active')
-        }
-        // Add .active class to accordion item
-        accordion[i].classList.add('active')
-
-        // Remove .active class from all content items
-        for (let i = 0; i < content.length; i++){
             content[i].classList.remove('active')
-        }
-        // Add .active class to content item
-        content[i].classList.add('active')
+
+        // Если класса нет, то
+        } else {
+            // Удалить класс .active у всех других accordion items
+            for (let i = 0; i < accordion.length; i++){
+                accordion[i].classList.remove('active')
+            }
+            // Добавить класс .active для accordion[i]
+            accordion[i].classList.add('active')
+            
+            // Удалить класс .active у всех других content items  
+            for (let i = 0; i < content.length; i++){
+                content[i].classList.remove('active')
+            }
+            // Добавить класс .active для content[i] 
+                content[i].classList.add('active')
+            }
     })
     }
-
-
-// var acc = document.getElementsByClassName("accordion-title");
-// var i;
-
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var panel = this.nextElementSibling;
-//     if (panel.style.maxHeight){
-//       panel.style.maxHeight = null;
-//     } else {
-//       panel.style.maxHeight = panel.scrollHeight + "px";
-//     } 
-//   });
-// }
