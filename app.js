06b9b20f0d28a94e920abdf30606a2f336e5f654
Rustam Kuzyaev <xpyct405@yandex.ru>
2023-03-09 15:52:21 +0300

    feat: use actual code && add description to code

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-let accordion = document.querySelectorAll('.accordion-title');
+let accordionBtn = document.querySelectorAll('.accordion-title');
 let content = document.querySelectorAll('.content');
 
-for (let i =0; i < accordion.length; i++){
-    accordion[i].addEventListener('click', function(){
+for (let i =0; i < accordionBtn.length; i++){
+    accordionBtn[i].addEventListener('click', function(){
 
-        // Проверка наличие класса .active у accordion[i], если есть то удалить .active у accordion[i] и content[i], чтоб по двойному клику вкладка открывалась и закрывалась
-        if (accordion[i].classList.contains('active')){
-            accordion[i].classList.remove('active')
+        // Проверка наличие класса .active у accordionBtn[i], если есть то удалить .active у accordionBtn[i] и content[i], чтоб по двойному клику вкладка открывалась и закрывалась
+        if (this.classList.contains('active')){
+            this.classList.remove('active')
             content[i].classList.remove('active')
 
         // Если класса нет, то
         } else {
-            // Удалить класс .active у всех других accordion items
-            for (let i = 0; i < accordion.length; i++){
-                accordion[i].classList.remove('active')
+            // Удалить класс .active у всех других accordionBtn items
+            for (let i = 0; i < accordionBtn.length; i++){
+                accordionBtn[i].classList.remove('active')
             }
-            // Добавить класс .active для accordion[i]
-            accordion[i].classList.add('active')
+            // Добавить класс .active для accordionBtn[i] именно той по которой кликаем
+            this.classList.add('active')
             
             // Удалить класс .active у всех других content items  
             for (let i = 0; i < content.length; i++){
@@ -24,6 +24,6 @@ for (let i =0; i < accordion.length; i++){
             }
             // Добавить класс .active для content[i] 
                 content[i].classList.add('active')
-            }
+        }
     })
-    }
+}
